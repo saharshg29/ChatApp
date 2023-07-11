e487970cb8c09285a2418c9f40ef8c9d501b51b1
Saharsh Gupta <saharshg29@gmail.com>
2023-07-12 00:54:45 +0530

    Setting up the frontend folder

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const chats = require("../backend/data/data");
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+  console.log("/api/chat is being called");
+});
+
+app.get("/api/chat/:id", (req, res) => {
+  let singleChat = chats.find((c) => (c._id = req.params.id));
+  res.send(singleChat)
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`server started at ${PORT}`));

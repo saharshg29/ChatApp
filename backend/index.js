433babd019024a9c92737d5017894bff0f9246cf
Saharsh Gupta <saharshg29@gmail.com>
2023-07-13 02:16:20 +0530

    Connection established with mongoDB

--- backend/index.js
+++ backend/index.js
@@ -3,24 +3,18 @@ const cors = require("cors")
 const dotenv = require("dotenv");
 const chats = require("../backend/data/data");
 const app = express();
-
+const userRoutes = require("./routes/userRoutes")
+const connectDB = require("./config/db")
 app.use(cors())
 
 dotenv.config();
 
+connectDB()
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/chat", (req, res) => {
-  res.send(chats);
-  console.log("/api/chat is being called");
-});
-
-app.get("/api/chat/:id", (req, res) => {
-  let singleChat = chats.find((c) => (c._id = req.params.id));
-  res.send(singleChat)
-});
+app.use("/api/user", userRoutes)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server started at ${PORT}`));

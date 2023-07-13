71e3b6b57d6a19e3e7cb4bf3d1c17fcdc7a788bf
Saharsh Gupta <saharshg29@gmail.com>
2023-07-13 23:16:54 +0530

    Signin and signup api done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -40,4 +40,22 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = {registerUser}
+
+const authUser = asyncHandler(async(req, res) => {
+  const {email, password} = req.body
+  const user = await User.findOne({email})
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email:user.email,
+      pic: user.pic,
+      token: generateToken(user._id)
+    })
+  }else {
+    res.status(201);
+    throw new Error("Invalid Email or password")
+  }
+})
+module.exports = {registerUser, authUser}

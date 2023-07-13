28b0787e55eb2d07ed13e618b6ea9e4af1693a62
Saharsh Gupta <saharshg29@gmail.com>
2023-07-13 23:17:08 +0530

    Error handling done

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,29 @@
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
 const dotenv = require("dotenv");
 const chats = require("../backend/data/data");
 const app = express();
-const userRoutes = require("./routes/userRoutes")
-const connectDB = require("./config/db")
-app.use(cors())
+const userRoutes = require("./routes/userRoutes");
+const connectDB = require("./config/db");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
+app.use(cors());
 
 dotenv.config();
 
-connectDB()
+connectDB();
 
-app.use(express.json()) // To allow acces to json
+app.use(express.json()); // To allow acces to json
 app.get("/", (req, res) => {
   res.send("API is running");
 });
+notFound;
 
 // including the db
 
+app.use("/api/user", userRoutes);
 
-app.use("/api/user", userRoutes)
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, console.log(`server started at ${PORT}`));

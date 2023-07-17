85cfeb7c70a98a7714b7461273734c257168defe
Saharsh Gupta <saharshg29@gmail.com>
2023-07-17 22:24:04 +0530

    Ready for production. Version 1.0

--- backend/index.js
+++ backend/index.js
@@ -9,15 +9,14 @@ const messageRoutes = require("./routes/messageRoutes");
 const connectDB = require("./config/db");
 const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 app.use(cors());
+const path = require("path");
 
 dotenv.config();
 
 connectDB();
 
 app.use(express.json()); // To allow acces to json
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+
 notFound;
 
 // including the db
@@ -25,6 +24,22 @@ notFound;
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
+
+// ----------------------------------DEPLOYEMENT------------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(__dirname1, "frontend", "build", "index.html");
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running");
+  });
+}
+// ----------------------------------DEPLOYEMENT------------------------------------
 
 app.use(notFound);
 app.use(errorHandler);
